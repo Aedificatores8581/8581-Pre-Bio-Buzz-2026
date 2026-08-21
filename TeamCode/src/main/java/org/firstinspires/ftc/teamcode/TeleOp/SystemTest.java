711e436c245ed013a86ce8682f2110fe2ce7229f
MCsweats <jjulianwynn029@gmail.com>
2026-08-20 21:57:22 -0400

    Create SystemTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/SystemTest.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "SystemTest", group = "TESTS")
+public class SystemTest extends OpMode {
+
+    @Override
+    public void init(){
+        telemetry.addData("Status", "Init");
+    }
+    public void loop(){
+        telemetry.addData("Status", "Play");
+    }
+
+}
